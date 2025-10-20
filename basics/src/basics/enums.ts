a9804f3a17b7fc6b52c80ba92a47018e36174b67
tokiored <jason@tokiored.com>
2025-10-20 15:03:11 +0100

    TS enums example

---
+++ basics/src/basics/enums.ts
@@ -0,0 +1,22 @@
+enum Priority {
+    Lowest = 0,
+    Low = 1,
+    Medium = 2,
+    High = 3,
+    Urgent = 4,
+}
+function priority(title: string, priority: number) {
+    if (priority === Priority.Lowest) {
+        console.log(`${title} is a ${Priority[priority]} priority`)
+    } else if (priority === Priority.Low) {
+        console.log(`${title} is a ${Priority[priority]} priority`)
+    } else if (priority === Priority.Medium) {
+        console.log(`${title} is a ${Priority[priority]} priority`)
+    } else if (priority === Priority.High) {
+        console.log(`${title} is a ${Priority[priority]} priority`)
+    } else if (priority === Priority.Urgent) {
+        console.log(`${title} is a ${Priority[priority]} priority`)
+    }
+}
+
+priority('Fix computer', 3)
